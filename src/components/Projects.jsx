0cc7386eca7a18379203cc678f75de816f8739f0
Tilak Garg <tilgarg21@gmail.com>
2025-12-12 20:51:38 +0530

    added AI-Code-Reviewer and GigFlow projects and added live links

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -15,23 +15,30 @@ const Projects = () => {
       <div>
         {PROJECTS.map((project, index) => (
           <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-            <motion.div
-              whileInView={{ opacity: 1, x: 0 }}
-              initial={{ opacity: 0, x: 100 }}
-              transtion={{ duration: 1 }}
-              className="w-full max-w-xl lg:w-3/4"
+            <a
+              href={project.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="block w-full max-w-xl lg:w-3/4 cursor-pointer"
             >
-              <h3 className="mb-2 font-semibold">{project.title}</h3>
-              <p className="mb-4 text-neutral-400">{project.description}</p>
-              {project.technologies.map((tech, index) => (
-                <span
-                  key={index}
-                  className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800"
-                >
-                  {tech}
-                </span>
-              ))}
-            </motion.div>
+              <motion.div
+                whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, x: 100 }}
+                transtion={{ duration: 1 }}
+                className="w-full max-w-xl lg:w-3/4"
+              >
+                <h3 className="mb-2 font-semibold">{project.title}</h3>
+                <p className="mb-4 text-neutral-400">{project.description}</p>
+                {project.technologies.map((tech, index) => (
+                  <span
+                    key={index}
+                    className="rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </motion.div>
+            </a>
           </div>
         ))}
       </div>
