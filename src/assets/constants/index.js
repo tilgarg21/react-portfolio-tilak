7c45eae4cec570834bbb8e2cacd0c9671ebc343e
Tilak Garg <tilgarg21@gmail.com>
2025-12-12 20:51:38 +0530

    added a new project and added links in projects

--- src/assets/constants/index.js
+++ src/assets/constants/index.js
@@ -3,16 +3,25 @@ export const ABOUT_TEXT = `I’m a Full Stack Developer who loves building smoot
 
 export const PROJECTS = [
   {
-    title: "CourseSeller",
+    title: "AI-Code-Reviewer",
+    description: 
+    "A web-based tool that lets users paste code and get AI-generated feedback/review instantly — combining an in-browser editor with a backend AI review engine to help developers catch issues or improve code quality",
+    technologies: ["React", "Vite", "JavaScript ES6+", "Prism.js", "CSS3", "Node.js", "Express.js"],
+    link: "https://ai-code-reviewer-ashy.vercel.app"
+  },
+  {
+    title: "Course-Seller",
     description:
     "A responsive web application that allows users to explore and view details of various online courses, with a  clean design and smooth navigation between pages.",
-    technologies: ["React", "Vite", "React Router", "Redux", "Javascript ES6+", "HTML5", "CSS3"],
+    technologies: ["React", "Vite", "Redux", "Javascript ES6+", "HTML5", "CSS3"],
+    link: "https://course-seller-khaki.vercel.app"
   },
   {
     title: "VideoTube-Backend",
     description:
     "A backend service that provides secure and efficient APIs for a video-sharing platform, managing data operations, user sessions, and integrations while serving as the backbone for a full-stack application.",
     technologies: ["Javascript ES6+", "Node.js", "Express.js", "REST APIs"],
+    link: "https://github.com/tilgarg21/videotube-backend"
   }
 ];
 
